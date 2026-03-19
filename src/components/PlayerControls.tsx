5d18c1044c116f0191423274a022953da53c23e8
chnoba <jsoliz@dc.uba.ar>
2026-03-19 19:41:06 -0300

    add aria labels

--- src/components/PlayerControls.tsx
+++ src/components/PlayerControls.tsx
@@ -8,12 +8,13 @@ import {useMusic} from "../contexts/MusicContext.tsx";
 export const PlayerControls = ({}) => {
     const iconSize = 24;
     const {next, prev, togglePlay, isPlaying} = useMusic();
-    const buttonClassname = "hover:brightness-125 transition-all active:translate-y-[1px]"
+    const buttonClassname = "transition-all active:translate-y-[1px]"
     return (
         <div className="flex items-center gap-2 origin-left">
             <button
                 className={buttonClassname}
                 onClick={prev}
+                aria-label="Previous Song"
             >
                 <Prev color="white" size={iconSize}/>
             </button>
@@ -21,6 +22,7 @@ export const PlayerControls = ({}) => {
             <button
                 className={buttonClassname}
                 onClick={togglePlay}
+                aria-label="Play Song"
             >
                 {isPlaying ? (
                     <Pause color="white" size={iconSize}/>
@@ -32,6 +34,7 @@ export const PlayerControls = ({}) => {
             <button
                 className={buttonClassname}
                 onClick={next}
+                aria-label="Next Song"
             >
                 <Next color="white" size={iconSize}/>
             </button>
